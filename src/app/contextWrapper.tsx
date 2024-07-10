774e112e898141f968d319c4b8fbf12d27c55045
Alvaro <pursuance@gmail.com>
2024-07-10 10:52:13 +0200

    Add recoil and react-query

---
+++ src/app/contextWrapper.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { RecoilRoot } from 'recoil';
+import { ReactNode } from 'react';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
+
+const RecoilRootWrapper = ({ children }: { children: ReactNode }) => (
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  </RecoilRoot>
+);
+
+export default RecoilRootWrapper;
