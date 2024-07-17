f6e95ae3dafe671dd4b35083142103a1cef733a0
Alvaro <pursuance@gmail.com>
2024-07-17 12:00:20 +0200

    Only show interaction when totally scrolled

--- src/components/globe-video/index.tsx
+++ src/components/globe-video/index.tsx
@@ -1,11 +1,11 @@
 'use client';
-import { useScroll, useMotionValueEvent } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useScroll, useMotionValueEvent, motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 export default function GlobeVideo() {
   const videoRef = useRef<HTMLVideoElement>(null);
   const { scrollYProgress } = useScroll();
-
+  const [interactionVisible, setInteractionVisible] = useState(false);
   useEffect(() => {
     if (!videoRef?.current) return;
     videoRef.current.pause();
@@ -14,6 +14,13 @@ export default function GlobeVideo() {
   useMotionValueEvent(scrollYProgress, "change", (latest) => {
     if (videoRef?.current?.readyState) {
       videoRef.current.currentTime = videoRef.current.duration * latest;
+    }
+    if (!interactionVisible && latest > 0.99) {
+      setInteractionVisible(true);
+    }
+
+    if (interactionVisible && latest < 0.99) {
+      setInteractionVisible(false);
     }
   })
 
@@ -27,9 +34,14 @@ export default function GlobeVideo() {
         preload="true"
         src="https://cdn.pixabay.com/video/2015/11/26/1393-147055573_small.mp4"
       ></video>
-      <div className="absolute h-full w-full z-50 inset-0">
+      {interactionVisible && <motion.div
+        className="absolute h-full w-full z-50 inset-0"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
         <span className="absolute top-[46%] left-[45%] h-3 w-3 rounded-full bg-yellow-500 hover:scale-150"></span>
-      </div>
+      </motion.div>}
     </>
   );
 }
