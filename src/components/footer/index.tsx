12eb91c28d1fd84e710e8675fc0e8da7bbc6c86e
Alvaro <pursuance@gmail.com>
2024-10-23 18:13:33 +0200

    Fix footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -10,7 +10,7 @@ const Footer = ({ backgroundClass = "bg-green-700" }) => {
       <div className="justify-center flex">
         <div className="w-1/2 px-[20px] xl:px-[100px] py-10 xl:py-20 flex flex-col gap-20">
           <div className="text-sm font-medium uppercase max-w-[258px] tracking-widest">Digital Twins: <div>Innovative Research for a Sustainable Future</div></div>
-          <Logo className="text-white" />
+          <Logo className="text-white w-40 xl:w-60" />
         </div>
         <div className="w-1/2 px-[20px] xl:px-[100px] py-10 xl:py-20 flex">
           <ul className="space-y-8">
@@ -32,9 +32,9 @@ const Footer = ({ backgroundClass = "bg-green-700" }) => {
           </ul>
         </div >
       </div>
-      <div className="px-[20px] xl:px-[100px] py-6 w-full gap-10 flex border-t border-white/20 text-white/50 text-lg leading-relaxed">
+      <div className="px-[20px] xl:px-[100px] py-6 w-full gap-10 flex border-t border-white/20 text-white/50 text-xs xl:text-lg leading-relaxed">
         <div className="max-w-[calc(50%-20px)]">© Barcelona Supercomputing Center, 2024</div>
-        <div className="xl:items-center gap-3 xl:gap-1 pt-2 xl:pt-0 flex-col xl:flex-row inline-flex text-sm xl:text-base">
+        <div className="xl:items-center gap-3 xl:gap-1 flex-col xl:flex-row inline-flex xl:text-base">
           <span>Designed and Developed by </span>
           <a
             href="https://www.vizzuality.com/"
