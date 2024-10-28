46d6ff27dd426a56715ac0107811d53dbddc4b05
Alvaro <pursuance@gmail.com>
2024-10-28 09:43:48 +0100

    Fix tablist on mobile

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md text-muted-foreground",
+      "inline-flex h-10 items-center text-muted-foreground max-w-full overflow-auto",
       className
     )}
     {...props}
