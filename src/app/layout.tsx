12037df3caebd0d8938231b709687746d8fe2d92
Alvaro <pursuance@gmail.com>
2024-11-07 13:12:54 +0100

    Fix mobile overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="overflow-x-hidden">
       <body className={cn(
         "min-h-screen bg-background font-sans antialiased scroll-smooth",
         fontSans.variable
