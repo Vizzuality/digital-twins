5db9cf54222bdc3e3ac9db41449c0818c8cd08eb
Alvaro <pursuance@gmail.com>
2024-11-11 17:36:30 +0100

    FIx padding

--- src/components/energy/intro/index.tsx
+++ src/components/energy/intro/index.tsx
@@ -8,7 +8,7 @@ export default function Intro() {
     <section className="relative bg-blue-950">
       <Lines verticalClassName="left-8 w-[calc(100vh-16px)]" sectionName='intro' columns={[64, 136, 452, 947]} rows={[220, 286, 302, 364, 468, 764]} />
       <div className='container relative pt-[110px] xl:pt-[177px] pl-8 xl:pl-24 flex flex-col-reverse xl:grid xl:grid-cols-[250px_1fr_250px] pb-[60px]'>
-        <div className='space-y-10 pt-[41px]'>
+        <div className='xl:space-y-10 xl:pt-[41px]'>
           {/* Text and images removed for now - May be recovered on the future */}
           {/* <div className='text-white text-balanced leading-[18px] pt-4 xl:pt-0'>
             The Climate Adaptation <br />Digital Twin in Action: <br />Climate simulations to support decisions in the energy sector.
