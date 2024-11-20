4a5ce3ca784fbf35aa76bf6f9cf0d7df9c49eca3
Alvaro <pursuance@gmail.com>
2024-11-20 14:02:04 +0100

    Fix responsive videos

--- src/components/home/section-4/index.tsx
+++ src/components/home/section-4/index.tsx
@@ -72,7 +72,8 @@ export default function Section4() {
       <div className="relative z-10 flex h-[255px] items-center justify-center overflow-hidden max-xl:mb-6 xl:h-[548px]">
         <VideoPlayer
           src="/videos/stream-videos/section-4/index.m3u8"
-          className="absolute z-0 w-screen object-fill"
+          className="absolute z-0 w-screen self-start min-h-[255px]"
+          videoClassName="object-cover sm:object-fit min-h-[255px]"
         />
         <div className="xl relative z-10 px-2 text-center text-white xl:px-0">
           <h2 className="text-center uppercase tracking-tight xl:text-lg">
